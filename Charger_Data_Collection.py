3b6fc55f8bf1a9d750c28e2b15eb23d6cdf2f1f4
arunvigneshram <67390217+arunvigneshram@users.noreply.github.com>
2022-07-06 22:36:48 +0530

    Add files via upload

---
+++ Charger_Data_Collection.py
@@ -0,0 +1,86 @@
+# import asyncio
+import pandas as pd
+import websocket
+import json
+import mysql.connector as myc
+import Charger_Configuration as config
+
+
+# Get IP address and generate the urls
+connections = []
+ips = []
+for charger in config.chargers:
+    connections.append(f"ws://{charger['ip']}:80/ws")
+    ips.append(charger["ip"])
+print(f"The IP Addresses of the chargers are, {ips}.")
+print(f"The Connections made from the IP addresses are, {connections}.")
+
+# Dataframe columns
+column_names = ["charger_id", "ip_address", "distributor", "capacity",
+                "charger_status", "low_state", "phases", "rfid", "last_seen"]
+
+while True:
+    # Create dataframe for every cycle
+    chargers_status_df = pd.DataFrame(columns=column_names)
+    print(chargers_status_df)
+
+    for i, connection in enumerate(connections):
+        # Recieve data from websocket
+        ws = websocket.create_connection(connection)
+        results = ws.recv()
+
+        # Process the recieved data form websocket
+        charger_data = json.loads("[" + results.replace("}\n{", "},\n{") + "]")
+
+        # Extract the required data in state variables
+        for item in charger_data:
+            if item["topic"] == "evse/state":
+                ice61851_state = item["payload"]["ice61851_state"]
+                charger_state = item["payload"]["charger_state"]
+                error_state = item["payload"]["error_state"]
+            if item["topic"] == "meter/phases":
+                phase_1 = item["payload"]["phases_connected"][0]
+                phase_2 = item["payload"]["phases_connected"][1]
+                phase_3 = item["payload"]["phases_connected"][2]
+            if item["topic"] == "nfc/seen_tags":
+                tag_id = item["payload"][0]["tag_id"]
+                last_seen = item["payload"][0]["last_seen"]
+
+        if phase_1 and phase_2 and phase_3:
+            phase_state = True
+
+        if ice61851_state == 1 and charger_state == 2 and error_state == 0 and phase_state:
+            ready_state = "READY"
+        else:
+            ready_state = "WAITING"
+
+        if phase_state == False:
+            ready_state = "FAILED"
+
+        chargers_status_df = chargers_status_df.append({"charger_id": config.chargers[i]["id"],
+                                                        "ip_address": config.chargers[i]["ip"],
+                                                        "distributor": config.chargers[i]["distributor"],
+                                                        "capacity": config.chargers[i]['current_limit'],
+                                                        "charger_status": ready_state,
+                                                        "low_state": None,
+                                                        "phases": phase_state,
+                                                        "rfid": tag_id,
+                                                        "last_seen": last_seen},
+                                                       ignore_index=True)
+
+    # chargers_status_df.set_index("charger_id", inplace=True)
+    print(chargers_status_df)
+
+    # Save dataframe as pickle
+    chargers_status_df.to_pickle("./chargers_status_df.pkl")
+
+# async def handle_socket(url, ):
+#     async with websockets.connect(url) as websocket:
+#         async for message in websocket:
+#             print(message)
+
+
+# async def handler():
+#     await asyncio.wait([handle_socket(uri) for uri in connections])
+
+# asyncio.get_event_loop().run_until_complete(handler())
